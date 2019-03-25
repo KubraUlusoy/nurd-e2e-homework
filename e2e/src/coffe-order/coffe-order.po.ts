595c5aa52511858a6fa258555756a56160d5de2c
KubraUlusoy <38496320+KubraUlusoy@users.noreply.github.com>
2019-03-25 14:14:12 +0300

    added functions for coffee.order.po.ts

--- e2e/src/coffe-order/coffe-order.po.ts
+++ e2e/src/coffe-order/coffe-order.po.ts
@@ -2,9 +2,49 @@ import { browser, by, element } from 'protractor';
 
 export class CoffeOrder {
   
+  navigateTo() {
+    return browser.get('/');
+  }
 
+  clickButton(buttonId) {
+    element(by.id(buttonId)).click();
+  }
 
+  getPriceText(){
+    return element(by.css('.price')).getText();
+  }
 
+  getBuyButtonStatus(){
+    return element(by.id('buy-button')).isEnabled();
+  }
+
+  getPageTitleText() {
+    return element(by.id('coffe-title')).getText();
+  }
+
+  clickAmericanoAddButton(){
+    return element(by.xpath('/html/body/app-root/app-coffe-order/div/div/div/div/div/div[1]/div/div/button')).click();
+  }
+
+  clickWhiteAddButton(){
+    return element(by.xpath('/html/body/app-root/app-coffe-order/div/div/div/div/div/div[2]/div/div/button')).click();
+  }
+
+  clickChaiteaAddButton(){
+    return element(by.xpath('/html/body/app-root/app-coffe-order/div/div/div/div/div/div[3]/div/div/button')).click();
+  }
+
+  clickLatteAddButton(){
+    return element(by.xpath('/html/body/app-root/app-coffe-order/div/div/div/div/div/div[4]/div/div/button')).click();
+  }
+
+  clickRemoveButton(){
+    return element(by.css('body > app-root > app-coffe-order > div > div > div > div > div:nth-child(5) > div:nth-child(1) > div > div > button')).click();
+  }
+
+  acceptAlert(){
+    return browser.switchTo().alert().accept();
+  }
 
   /**
    * 
